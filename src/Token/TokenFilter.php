41e1ef3fd91bf56a0f76714f1dcc5abbf040847a
adammarton <adam_marton@epam.com>
2019-02-09 14:57:01 +0100

    introducing iterator

---
+++ src/Token/TokenFilter.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace AdamMarton\Stub\Token;
+
+final class TokenFilter
+{
+    /**
+     * @var array
+     */
+    private $tokens = [];
+
+    /**
+     * @param  array $tokens
+     */
+    public function __construct(array $tokens)
+    {
+        $this->tokens = $tokens;
+    }
+
+    /**
+     * @return array $filtered
+     */
+    public function filter() : array
+    {
+        $filtered   = [];
+        $unfiltered = $this->tokens;
+
+        foreach ($unfiltered as $token) {
+            if (is_string($token)) {
+                $token = [
+                    999999,
+                    $token
+                ];
+            }
+
+            $token[1] = $this->sanitize($token[1]);
+
+            if (!empty(trim($token[1])) || is_numeric($token[1])) {
+                $filtered[] = array_slice($token, 0, 2);
+            }
+        }
+
+        return $filtered;
+    }
+
+    /**
+     * @param  string $token
+     * @return string
+     */
+    private function sanitize(string $token) : string
+    {
+        return str_replace(["\n", '//'], [''], trim($token));
+    }
+}
